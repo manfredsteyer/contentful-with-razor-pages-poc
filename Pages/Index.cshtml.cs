a3c64940026a82ee39cd86eb4a82999305332055
Manfred Steyer <manfred.steyer@gmx.net>
2019-08-12 20:51:55 +0200

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contentful.Core;
+using Contentful.Core.Models;
+using Contentful.Core.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+
+
+namespace RazorPagesMovie.Pages
+{
+
+    public class Blog
+    {
+        public Contentful.Core.Models.SystemProperties Sys { get; set; }
+        public string Title { get; set; }
+        public string Info { get; set; }
+        public Contentful.Core.Models.Document Text { get; set; }
+
+
+        public List<Asset> Image { get; set; }
+
+    }
+
+    public class IndexModel : PageModel
+    {
+        private IContentfulClient client;
+        IConfiguration configuration;
+
+        public Blog Blog { get; set; }
+
+        public IndexModel(IConfiguration configuration, IContentfulClient client, IOptions<ContentfulOptions> contentfulOptions)
+        {
+            this.client = client;
+            this.configuration = configuration;
+        }
+        public async Task OnGetAsync()
+        {
+            var entries = await client.GetEntriesByType<Blog>("blog");
+            this.Blog = entries.First();
+        }
+
+        private class Debugger
+        {
+        }
+    }
+}
